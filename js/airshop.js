d602bc6598c45dd777d616b1f626e81f2d387642
Rost Yanevych <Rostyslav.Yanevych@edvantis.com>
2015-07-28 14:03:21 +0300

    Update. jquery.mobile removed -- seems that touch events are not triggered on device

--- js/airshop.js
+++ js/airshop.js
@@ -11,10 +11,11 @@ function showMenu() {
         <div class="text-center orange">Unloaded</div> \
         <div class="container pad10">\
          <a class="menu-item text-center"><span class="menu-icon tools-icon"></span><div class="menu-icon-text">Tools</div></a>\
-         <a class="menu-item text-center"><span class="menu-icon suspend-icon"></span><div class="menu-icon-text">Suspend</div></a>\
+         <a class="menu-item text-center" onclick="YTsystem.suspend();"><span class="menu-icon suspend-icon"></span><div class="menu-icon-text">Suspend</div></a>\
          <a class="menu-item text-center"><span class="menu-icon calc-icon"></span><div class="menu-icon-text">Calculator</div></a>\
          <a class="menu-item text-center"><span class="menu-icon about-icon"></span><div class="menu-icon-text">About</div></a>\
          <a class="menu-item text-center"><span class="menu-icon nuke-icon"></span><div class="menu-icon-text">Clear Down</div></a>\
+         <a href="buttons_test.html" class="menu-item text-center"><span class="menu-icon nuke-icon"></span><div class="menu-icon-text">Buttons test</div></a>\
         </div>\
         <footer> \
         <button class="btn pink w30" onclick="hideMenu();">Back</button> \
@@ -38,6 +39,12 @@ $(document).on('click', 'button[href]', function(e) {
     //e.stopPropagation();
     console.log('click on button with href', e);
     location.href = $(e.currentTarget).attr('href');
+}).on('mousedown', '.btn', function(e) {
+    //console.log('touchstart mousedown', e.currentTarget);
+    $(e.currentTarget).addClass('pressed');
+}).on('mouseup', '.btn', function (e) {
+    //console.log('touchend mouseup', e.currentTarget);
+    $(e.currentTarget).removeClass('pressed');
 });
 
-document.body.addEventListener('touchstart', function() {}, false);
+//document.body.addEventListener('touchstart', function() {}, false);
